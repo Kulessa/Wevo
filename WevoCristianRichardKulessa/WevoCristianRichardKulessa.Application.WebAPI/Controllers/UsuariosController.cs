1e781a81dc12c70d1bc329fe43b284bf0b247ff7
Cristian Richard Kulessa <kulessa@gmail.com>
2019-11-12 17:15:50 -0300

    Criação do Projeto da WebAPI

---
+++ WevoCristianRichardKulessa/WevoCristianRichardKulessa.Application.WebAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WevoCristianRichardKulessa.Application.WebAPI.Models;
+
+namespace WevoCristianRichardKulessa.Application.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        public UsuariosController(){}
+        [HttpGet]
+        public Task<ActionResult<IEnumerable<UsuarioModel>>> GetUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpGet("{id}")]
+        public Task<ActionResult<UsuarioModel>> GetUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut("{id}")]
+        public Task<ActionResult<UsuarioModel>> PutUsuario(int id, UsuarioModel usuario)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost()]
+        public Task<ActionResult<UsuarioModel>> PostUsuario(UsuarioModel usuario)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete("{id}")]
+        public Task<ActionResult<UsuarioModel>> DeleteUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WevoCristianRichardKulessa/WevoCristianRichardKulessa.Application.WebAPI/Models/UsuarioModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WevoCristianRichardKulessa.Application.WebAPI.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string EMail { get; set; }
+        public string Sexo { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
